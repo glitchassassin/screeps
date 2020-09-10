c48bf6efbcb08cdf2b18c5c4bdebc2ea664bcfd0
Jon Winsley <jon.winsley@gmail.com>
2020-09-10 11:41:41 -0400

    Depositing in containers

--- src/behaviors/deposit.ts
+++ src/behaviors/deposit.ts
@@ -3,8 +3,8 @@ export const deposit = (creep: Creep) => {
         filter: (structure: Structure) => {
             return (structure.structureType == STRUCTURE_EXTENSION ||
                     structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_TOWER) &&
-                    (structure as StructureExtension|StructureSpawn|StructureTower).store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    structure.structureType == STRUCTURE_CONTAINER) &&
+                    ((structure as StructureExtension|StructureSpawn|StructureContainer).store as Store<RESOURCE_ENERGY, false>).getFreeCapacity(RESOURCE_ENERGY) > 0;
         }
     });
     if (!target) return false; // No storage available - default to next priority role
