0f7a2b64ddf078b8229746d514985c23d3dc00b2
Jon Winsley <jon.winsley@gmail.com>
2020-09-24 13:44:42 -0400

    Fixed controller spawning uncontrollably

--- src/managers/ControllerManager.ts
+++ src/managers/ControllerManager.ts
@@ -5,6 +5,7 @@ import { WithdrawTask } from "tasks/types/WithdrawTask";
 import { TaskRequest } from "tasks/TaskRequest";
 import { Task } from "tasks/Task";
 import { TransferTask } from "tasks/types/TransferTask";
+import { TravelTask } from "tasks/types/TravelTask";
 
 export class ControllerManager extends Manager {
     upgraders: Creep[] = [];
@@ -26,7 +27,7 @@ export class ControllerManager extends Manager {
                 (room.controller.level === 8 ? 15 : 1000) // TODO: Cap of 15 energy per tick at RCL 8, this is capacity per task
             ));
         } else {
-            if (global.analysts.statistics.metrics[room.name].controllerDepotFillRate.mean() >= 0) {
+            if (Game.time % 50 === 0 && global.analysts.statistics.metrics[room.name].controllerDepotFillRate.mean() >= 0) {
                 // More input than output: spawn more upgraders
                 global.supervisors[room.name].spawn.submit(new MinionRequest(room.controller.id, 4, MinionTypes.UPGRADER))
             }
@@ -38,17 +39,26 @@ export class ControllerManager extends Manager {
             if (global.supervisors[room.name].task.isIdle(upgrader)) {
                 if(upgrader.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
                     // Upgrader has energy - dump it into controller
-                    global.supervisors[room.name].task.assign(new Task([new UpgradeTask(room.controller)], upgrader, room.name));
+                    global.supervisors[room.name].task.assign(new Task([
+                        new TravelTask((room.controller as StructureController).pos, 3),
+                        new UpgradeTask(room.controller)
+                    ], upgrader, room.name));
                 } else {
                     // Upgrader needs energy - get from controller container, preferably
                     let depot = global.analysts.controller.getDesignatedUpgradingLocations(room);
                     if (depot && depot.container) {
-                        global.supervisors[room.name].task.assign(new Task([new WithdrawTask(depot.container)], upgrader, room.name));
+                        global.supervisors[room.name].task.assign(new Task([
+                            new TravelTask(depot.container.pos, 1),
+                            new WithdrawTask(depot.container)
+                        ], upgrader, room.name));
                     } else {
                         // No upgrader depot exists yet; see if there's a spawn we can withdraw from instead
                         let spawn = global.analysts.spawn.getSpawns(room).find(s => s.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
                         if (spawn) {
-                            global.supervisors[room.name].task.assign(new Task([new WithdrawTask(spawn)], upgrader, room.name));
+                            global.supervisors[room.name].task.assign(new Task([
+                                new TravelTask(spawn.pos, 1),
+                                new WithdrawTask(spawn)
+                            ], upgrader, room.name));
                         }
                     }
                 }
