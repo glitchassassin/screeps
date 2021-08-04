c40ef97bde49668e976c83854acc65599c24a2b6
Jon Winsley <jon.winsley@gmail.com>
2021-08-04 15:38:10 -0400

    Setting territory radius for shard3

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 /**
  * Rooms around an Office to control as remote territories
  */
-export const TERRITORY_RADIUS = 1;
+export const TERRITORY_RADIUS = Game.shard.name === 'shard3' ? 0 : 1;
 
 /**
  * Barrier level targets by RCL
