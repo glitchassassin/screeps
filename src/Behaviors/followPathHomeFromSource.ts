f58947a7f48162733919bec7fb4517d03b4faa93
Jon Winsley <jon.winsley@gmail.com>
2023-07-05 12:17:52 -0400

    Fixing bucket brigade issue

--- src/Behaviors/followPathHomeFromSource.ts
+++ src/Behaviors/followPathHomeFromSource.ts
@@ -1,6 +1,7 @@
-import { moveByPath, moveTo } from 'screeps-cartographer';
 import { franchiseRoadsToBuild } from 'Selectors/plannedFranchiseRoads';
 import { roomPlans } from 'Selectors/roomPlans';
+import { viz } from 'Selectors/viz';
+import { CachingStrategies, Keys, getCachedPath, moveByPath, moveTo } from 'screeps-cartographer';
 import { BehaviorResult } from './Behavior';
 
 export const followPathHomeFromSource = (creep: Creep, office: string, sourceId: Id<Source>) => {
@@ -12,3 +13,38 @@ export const followPathHomeFromSource = (creep: Creep, office: string, sourceId:
   }
   return BehaviorResult.INPROGRESS;
 };
+
+export function isCloserToDestination(origin: Creep, target: Creep, office: string, sourceId?: Id<Source>) {
+  // default to "go home" path
+  let creepPath = getCachedPath(office + sourceId);
+  if (!sourceId || origin.pos.roomName === office || franchiseRoadsToBuild(office, sourceId).length || !creepPath) {
+    // use creep-cached path instead
+    creepPath = getCachedPath(Keys.creepKey(origin, '_cp'), { cache: CachingStrategies.HeapCache })
+  }
+
+  if (!creepPath) {
+    return false; // no cached path found
+  }
+
+  const currentPos = creepPath.findIndex(t => t.isEqualTo(origin.pos))
+
+  if (currentPos === -1 || currentPos === creepPath.length - 1) {
+    return false; // not on path, or at end of path
+  }
+
+  // if target is on the next step, it is closer
+  if (creepPath[currentPos + 1].isEqualTo(target.pos)) {
+    return true;
+  }
+
+  const targetSquare = creepPath[currentPos + 2];
+
+  if (targetSquare) viz(origin.pos.roomName).line(origin.pos.x, origin.pos.y, targetSquare.x, targetSquare.y, { color: "green" })
+
+  if (targetSquare && target.pos.getRangeTo(targetSquare) < origin.pos.getRangeTo(targetSquare)) {
+    // the other creep is closer to the destination along our current path
+    return true;
+  }
+  // the other creep is not closer or it doesn't matter
+  return false;
+}
